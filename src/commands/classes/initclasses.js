622d23c10e38958b04473899f50302760e08d141
Fabian <info@maierfabian.de>
2024-04-15 21:52:05 +0200

    added the class init (will be removed after first use)

---
+++ src/commands/classes/initclasses.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { db } = require("../../lib/db");
+module.exports = {
+    run: async ({ interaction }) => {
+        // Rolle ID, nach der gesucht werden soll
+        const roleId = '1227726730404565135'; // Replace with the actual role ID
+
+        // Finde die Rolle im Server
+        const role = interaction.guild.roles.cache.get(roleId);
+
+        // Überprüfe, ob die Rolle existiert
+        if (!role) {
+            return interaction.channel.send(`Die Rolle mit ID ${roleId} existiert nicht.`);
+        }
+
+        // Finde alle Benutzer mit der Rolle
+        const usersWithRole = interaction.guild.members.cache.filter(member => member.roles.cache.has(role.id));
+
+        // Füge die Benutzer zur Datenbank hinzu
+        usersWithRole.forEach(async user => {
+            const newDBUser = await db.users.create({
+                data: {
+                    userId: user.id,
+                    username: user.user.username,
+                    rssclass: 'Deine Klasse'
+                }
+            })
+
+            // Füge den Benutzer zur Datenbank hinzu
+            db.addUser(userId, username, userClass);
+        });
+
+        interaction.channel.send(`Alle Benutzer mit der Rolle ${role.name} wurden zur Datenbank hinzugefügt.`);
+    },
+    data: new SlashCommandBuilder().setName("init").setDescription("This command initializes the classes in the database. Only devs")
+};
