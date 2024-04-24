ea15ab53c4f4e6b37e653b441fbc7e5153c04f26
Fabian <info@maierfabian.de>
2024-04-24 20:14:39 +0200

    fixed issue #34

--- src/events/guildCreate/register.js
+++ src/events/guildCreate/register.js
@@ -1,5 +1,6 @@
 module.exports =  async (guild) => {
     const { db } = require("../../lib/db");
+const { EmbedBuilder } = require('discord.js');
     const guildDB = await db.guilds.findFirst({
         where: {
             guildId: guild.id
@@ -20,5 +21,22 @@ module.exports =  async (guild) => {
         data: {
             guildId: guild.id,
         }
-    })}}
+
+    })
+    const owner = await guild.fetchOwner();
+
+    const embed = new EmbedBuilder()
+  .setTitle("Hey there!")
+  .setURL("https://scrimfinder.de")
+  .setDescription("Thanks for adding me to your server! \nI autoregisterd your guild so you don't have to use the /register command.\nIf you need help, visit our [docs](https://docs.scrimfinder.de) or join our [Support Server](https://discord.gg/division-league-833783529506078781)")
+  .setThumbnail("https://maierfabian.de/images/hipingu.png")
+  .setColor("#ff7700")
+  .setFooter({
+    text: "Scrimfinder.de | Finding Scrims was never that easy!",
+    iconURL: "https://maierfabian.de/images/lovepingu.png",
+  })
+  .setTimestamp();
+    owner.send({embeds: [embed]});
+    
+}}
 }
