45470c5b9ccb8443df11f6d07e2af4679025eb7c
Fabian <info@maierfabian.de>
2024-04-27 23:14:04 +0200

    autodelete of messages in the db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
  //Imports 
 const { Client, IntentsBitField } = require('discord.js');
 const { CommandHandler } = require('djs-commander');
+const { db } = require("./lib/db");
 const path = require('path');
 require('dotenv').config();
 
@@ -16,6 +17,25 @@ const client = new Client({
     ]
 });
 
+async function deleteOldMessages() {
+    // Get the current date and time
+    const now = new Date();
+
+    // Subtract 3 days from the current date and time
+    now.setDate(now.getDate() - 3);
+
+    // Delete all messages that are older than 3 days
+    await db.message.deleteMany({
+        where: {
+            createdAt: {
+                lte: now,
+            },
+        },
+    });
+}
+
+// Run the deleteOldMessages function every hour
+setInterval(deleteOldMessages, 60 * 60 * 1000);
 
 // Create CommandHandler
 new CommandHandler({
@@ -24,6 +44,8 @@ new CommandHandler({
     eventsPath: path.join(__dirname, 'events'),
     validationsPath: path.join(__dirname, 'validations')
 });
+
+
 
 // Bot login
 (async () => {
