78b2baaf0d12342e0a01df6127c021dd6f772b34
Fabian <info@maierfabian.de>
2024-05-08 21:50:00 +0200

    optimized

--- src/commands/scrimsearch/findscrim.js
+++ src/commands/scrimsearch/findscrim.js
@@ -1,10 +1,4 @@
-const {
-  SlashCommandBuilder,
-  EmbedBuilder,
-  ButtonBuilder,
-  ButtonStyle,
-  ActionRowBuilder,
-} = require("discord.js");
+const {SlashCommandBuilder,EmbedBuilder,ButtonBuilder,ButtonStyle,ActionRowBuilder,} = require("discord.js");
 const { db } = require("../../lib/db");
 const { BANNED_USER_MESSAGE } = require("../../constants/banned");
 
@@ -13,7 +7,7 @@ module.exports = {
     const game = interaction.options.getString("game");
     const date = interaction.options.getString("date");
     const time = interaction.options.getString("time");
-    const bestof = interaction.options.getString('best-of');
+    const bestof = interaction.options.getString("best-of");
     let teamname = interaction.options.getString("team-name");
     let extrainfo = interaction.options.getString("extra-info");
     let shareranges = interaction.options.getString("shareit-everywhere");
@@ -23,7 +17,7 @@ module.exports = {
     const sentGuildIds = [];
 
     try {
-      maps = parseInt(interaction.options.getString('best-of'));
+      maps = parseInt(interaction.options.getString("best-of"));
     } catch (err) {
       console.log(err);
     }
@@ -34,11 +28,16 @@ module.exports = {
       });
 
       if (userBanned) {
-        await interaction.reply({ embeds: [BANNED_USER_MESSAGE], ephemeral: true });
+        await interaction.reply({
+          embeds: [BANNED_USER_MESSAGE],
+          ephemeral: true,
+        });
         return;
       }
 
-      let userInDB = await db.users.findFirst({ where: { userId: interaction.user.id } });
+      let userInDB = await db.users.findFirst({
+        where: { userId: interaction.user.id },
+      });
 
       if (!userInDB) {
         userInDB = await db.users.create({
@@ -51,24 +50,27 @@ module.exports = {
         rank = "I";
         const welcome = new EmbedBuilder()
           .setTitle("Welcome to Scrimfinder!")
-          .setDescription("Welcome in the Scrimfinder Family! \n\nYou can now start finding scrims with the command /findscrim\n\nIf you have any questions feel free to ask in our [Discord](https://discord.gg/nrbwf929rK) or visit our [Website](https://scrimfinder.gg)")
-          .addFields(
-            {
-              name: "What are Classes?",
-              value: "Scrimfinder uses Classes to determine the skill level of the players. \n\n**Class I** is the lowest class and **Class A** is the highest class. \n\nYour class gets determined by your teams achivements in leagues and tournaments. Check out our discord for more information.",
-              inline: true,
-            },
+          .setDescription(
+            "Welcome in the Scrimfinder Family! \n\nYou can now start finding scrims with the command /findscrim\n\nIf you have any questions feel free to ask in our [Discord](https://discord.gg/nrbwf929rK) or visit our [Website](https://scrimfinder.gg)"
           )
+          .addFields({
+            name: "What are Classes?",
+            value:
+              "Scrimfinder uses Classes to determine the skill level of the players. \n\n**Class I** is the lowest class and **Class A** is the highest class. \n\nYour class gets determined by your teams achivements in leagues and tournaments. Check out our discord for more information.",
+            inline: true,
+          })
           .setColor("#ff7700")
           .setThumbnail("https://maierfabian.de/images/hipingu.png")
           .setFooter({
             text: "scrimfinder.gg | Finding Scrims was never that easy",
             iconURL: "https://maierfabian.de/images/lovepingu.png",
-          })
+          });
         try {
-          await interaction.user.send({embeds: [welcome]});
+          await interaction.user.send({ embeds: [welcome] });
         } catch (err) {
-          console.log(`Failed to send DM to user: ${interaction.user.id}. Error: ${err.message}`);
+          console.log(
+            `Failed to send DM to user: ${interaction.user.id}. Error: ${err.message}`
+          );
         }
       } else {
         rank = userInDB.rssclass;
@@ -83,108 +85,139 @@ module.exports = {
       console.log(err);
       return;
     }
-try{
-  await interaction.deferReply({ ephemeral: true });
-    const send = new EmbedBuilder()
-      .setTitle("Scrimsearch started!")
-      .setURL("https://docs.scrimfinder/invite")
-      .setDescription("Have fun and make sure that your DM's are open.\nIf you haven't already consider to invite me to your Server!")
-      .setColor("#ff7700")
-      .setFooter({
-        text: "scrimfinder.gg | Finding Scrims was never that easy",
-        iconURL: "https://maierfabian.de/images/lovepingu.png",
-      })
-      .setTimestamp();
-
-    const inv = constructInviteButton();
-
-    if (!teamname) {
-      teamname = "";
-    }
-
-    const contactRow = constructContactRow(interaction.user);
-    if (extrainfo === null) {
-      extrainfo = "";
-    }
-
     try {
-      if (game === "rss") {
-        if (rank === "I" || rank === "H" || rank === "G") {
-          const channels = await getChannelsForScrim(rank);
+      await interaction.deferReply({ ephemeral: true });
+      const send = new EmbedBuilder()
+        .setTitle("Scrimsearch started!")
+        .setURL("https://docs.scrimfinder/invite")
+        .setDescription(
+          "Have fun and make sure that your DM's are open.\nIf you haven't already consider to invite me to your Server!"
+        )
+        .setColor("#ff7700")
+        .setFooter({
+          text: "scrimfinder.gg | Finding Scrims was never that easy",
+          iconURL: "https://maierfabian.de/images/lovepingu.png",
+        })
+        .setTimestamp();
 
-          if (channels.length) {
-            const scrimsearchEmbed = constructScrimsearchEmbed(interaction.user, date, time, bestof, extrainfo, rank);
+      const inv = constructInviteButton();
 
-            for (const c of channels) {
-              const message = await sendMessageToChannel(client, c, scrimsearchEmbed, contactRow);
-if (message) {
-  sentMessageIds.push(message.id);
-  sentChannelIds.push(c);
-  if (interaction.guild) {
-    sentGuildIds.push(interaction.guild.id);
-  }
-}
+      if (!teamname) {
+        teamname = "";
+      }
+
+      const contactRow = constructContactRow(interaction.user);
+      if (extrainfo === null) {
+        extrainfo = "";
+      }
+
+      try {
+        if (game === "rss") {
+          if (rank === "I" || rank === "H" || rank === "G") {
+            const channels = await getChannelsForScrim(rank);
+
+            if (channels.length) {
+              const scrimsearchEmbed = constructScrimsearchEmbed(
+                interaction.user,
+                date,
+                time,
+                bestof,
+                extrainfo,
+                rank
+              );
+
+              for (const c of channels) {
+                const message = await sendMessageToChannel(
+                  client,
+                  c,
+                  scrimsearchEmbed,
+                  contactRow
+                );
+                if (message) {
+                  sentMessageIds.push(message.id);
+                  sentChannelIds.push(c);
+                  if (interaction.guild) {
+                    sentGuildIds.push(interaction.guild.id);
+                  }
+                }
+              }
             }
-          }
-        } else {
-          const channels = await getChannelsForGSAScrim();
+          } else {
+            const channels = await getChannelsForGSAScrim();
 
-          if (channels.length) {
-            const scrimsearchEmbed = constructScrimsearchEmbed(interaction.user, date, time, bestof, extrainfo, rank);
+            if (channels.length) {
+              const scrimsearchEmbed = constructScrimsearchEmbed(interaction.user, date, time, bestof, extrainfo, rank);
 
-            for (const c of channels) {
-              const message = await sendMessageToChannel(client, c, scrimsearchEmbed, contactRow);
-              if (message) {
-                sentMessageIds.push(message.id);
-                sentChannelIds.push(c);
-                if (interaction.guild) {
-                  sentGuildIds.push(interaction.guild.id);
+              for (const c of channels) {
+                const message = await sendMessageToChannel(client, c, scrimsearchEmbed, contactRow);
+                if (message) {
+                  sentMessageIds.push(message.id);
+                  sentChannelIds.push(c);
+                  if (interaction.guild) {
+                    sentGuildIds.push(interaction.guild.id);
+                  }
                 }
               }
             }
           }
-        }
 
-        if (shareranges === "yes") {
-          const channels = await getChannelsForSharedScrim(rank);
-        
-          if (channels.length) {
-            const scrimsearchEmbed = constructScrimsearchEmbed(interaction.user, date, time, bestof, extrainfo, rank);
-        
-            for (const c of channels) {
-              const message = await sendMessageToChannel(client, c, scrimsearchEmbed, contactRow);
-              sentMessageIds.push(message.id);
-              sentChannelIds.push(c);
-              sentGuildIds.push(interaction.guild.id);
+          if (shareranges === "yes") {
+            const channels = await getChannelsForSharedScrim(rank);
+
+            if (channels.length) {
+              const scrimsearchEmbed = constructScrimsearchEmbed(
+                interaction.user,
+                date,
+                time,
+                bestof,
+                extrainfo,
+                rank
+              );
+
+              for (const c of channels) {
+                const message = await sendMessageToChannel(
+                  client,
+                  c,
+                  scrimsearchEmbed,
+                  contactRow
+                );
+                sentMessageIds.push(message.id);
+                sentChannelIds.push(c);
+                sentGuildIds.push(interaction.guild.id);
+              }
             }
           }
         }
+      } catch (err) {
+        console.log(err);
+        return;
       }
-    } catch (err) {
-      console.log(err);
-      return;
-    }
 
-    try {
-      await db.message.create({
-        data: {
-          content: ` **${date} ${time} **     **Class ${rank}**     **${bestof} Maps**`,
-          messageIds: sentMessageIds,
-          channelIds: sentChannelIds,
-          guildIds: sentGuildIds,
-          user: {
-            connect: {
-              userId: interaction.user.id,
+      try {
+        await db.message.create({
+          data: {
+            content: ` **${date} ${time} **     **Class ${rank}**     **${bestof} Maps**`,
+            messageIds: sentMessageIds,
+            channelIds: sentChannelIds,
+            guildIds: sentGuildIds,
+            user: {
+              connect: {
+                userId: interaction.user.id,
+              },
             },
           },
-        },
-      });
+        });
 
-      await interaction.editReply({ embeds: [send], components: [inv], ephemeral: true });
+        await interaction.editReply({
+          embeds: [send],
+          components: [inv],
+          ephemeral: true,
+        });
+      } catch (err) {
+        console.log(err);
+        await interaction.editReply({ embeds: [send], components: [inv] });
+      }
     } catch (err) {
-      console.log(err);
-      await interaction.editReply({ embeds: [send], components: [inv] });
-    }}catch(err){
       console.log(err);
     }
   },
@@ -203,13 +236,21 @@ if (message) {
       option.setName("best-of").setDescription("Best of.").setRequired(true)
     )
     .addStringOption((option) =>
-      option.setName("date").setDescription("The date you want to find a scrim for.").setRequired(true)
+      option
+        .setName("date")
+        .setDescription("The date you want to find a scrim for.")
+        .setRequired(true)
     )
     .addStringOption((option) =>
-      option.setName("time").setDescription("The time you want to scrim.").setRequired(true)
+      option
+        .setName("time")
+        .setDescription("The time you want to scrim.")
+        .setRequired(true)
     )
     .addStringOption((option) =>
-      option.setName("extra-info").setDescription("Extra information about the scrim.")
+      option
+        .setName("extra-info")
+        .setDescription("Extra information about the scrim.")
     )
     .addStringOption((option) =>
       option
@@ -227,13 +268,17 @@ async function sendMessageToChannel(client, channelId, embed, components) {
   const channelToSend = client.channels.cache.get(channelId);
   if (channelToSend) {
     try {
-      return await channelToSend.send({ embeds: [embed], components: [components] });
+      return await channelToSend.send({
+        embeds: [embed],
+        components: [components],
+      });
     } catch (err) {
-      console.log(`Failed to send message to channel: ${channelId}. Error: ${err.message}`);
+      console.log(
+        `Failed to send message to channel: ${channelId}. Error: ${err.message}`
+      );
     }
-  }else{
+  } else {
     console.log(`Channel ${channelId} not found`);
-  
   }
 }
 
@@ -244,7 +289,12 @@ async function getChannelsForScrim(rank) {
   guilds.forEach((guild) => {
     if (guild.rssGtoIid && (rank === "I" || rank === "H" || rank === "G")) {
       channels.push(...guild.rssGtoIid);
-    } else if (guild.rssDtoFid && (rank !== "I" && rank !== "H" && rank !== "G")) {
+    } else if (
+      guild.rssDtoFid &&
+      rank !== "I" &&
+      rank !== "H" &&
+      rank !== "G"
+    ) {
       channels.push(...guild.rssDtoFid);
     }
   });
@@ -304,7 +354,9 @@ function constructScrimsearchEmbed(user, date, time, bestof, extrainfo, rank) {
       iconURL: `http://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`,
       url: `https://discordapp.com/users/${user.id}/`,
     })
-    .setDescription(` **${date} ${time}** |     **Class ${rank}** |     **${bestof} Maps**`)
+    .setDescription(
+      ` **${date} ${time}** |     **Class ${rank}** |     **${bestof} Maps**`
+    )
     .setColor("#ff7700")
     .setFooter({
       text: `Scrimfinder.gg  | ${user.id}`,
