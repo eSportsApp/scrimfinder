5b4a84f69700d2bb7508b143541e39c51bcff14e
Fabian <info@maierfabian.de>
2024-11-06 21:25:51 +0100

    fixed deployment errors

--- src/events/_start/interactionHandler.ts
+++ src/events/_start/interactionHandler.ts
@@ -3,10 +3,10 @@ import path from 'node:path'
 import { pathToFileURL } from 'node:url'
 
 const ButtonsDir = path.join(process.cwd(), '.robo', 'build', 'buttons')
-const ModalsDir = path.join(process.cwd(), '.robo', 'build', 'modals')
+//const ModalsDir = path.join(process.cwd(), '.robo', 'build', 'modals')
 
 export const ButtonHandlers = new Map<string, any>()
-export const ModalHandlers = new Map<string, any>()
+//export const ModalHandlers = new Map<string, any>()
 
 /*
  * Load interaction handlers in advance for future use
@@ -18,12 +18,12 @@ export default async () => {
 			const fileURL = pathToFileURL(filePath).href
 			const handler = await import(fileURL)
 			ButtonHandlers.set(handler.customID, handler.default)
-		}),
+		})/*,
 		...readdirSync(ModalsDir).map(async (fileName) => {
 			const filePath = path.join(ModalsDir, fileName)
 			const fileURL = pathToFileURL(filePath).href
 			const handler = await import(fileURL)
 			ModalHandlers.set(handler.customID, handler.default)
-		})
+		})*/
 	])
 }
